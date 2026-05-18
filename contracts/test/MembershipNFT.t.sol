c167781bd2ee9d1957575e1205e58cb1227e7b1c
Darren Mckeeman <darren.mckeeman@gmail.com>
2026-05-18 14:31:05 -0700

    test(02-01): extend MembershipNFT unit tests
    - Add burn authorization tests (holder, owner, stranger-revert)
    - Add memberSince cleared on burn
    - Add setJoinPolicy and setMetadataURI owner-only tests
    - Add ERC721Votes delegation to self and third-party
    - Add mintTo non-owner revert test
    - Add Application policy behaves like Allowlist test

--- contracts/test/MembershipNFT.t.sol
+++ contracts/test/MembershipNFT.t.sol
@@ -46,4 +46,81 @@ contract MembershipNFTTest is Test {
         nft.mintTo(alice);
         assertEq(nft.balanceOf(alice), 1);
     }
+
+    function test_BurnByHolder() public {
+        vm.prank(alice);
+        uint256 id = nft.joinOpen();
+        vm.prank(alice);
+        nft.burn(id);
+        assertEq(nft.balanceOf(alice), 0);
+        assertEq(nft.memberSince(alice), 0);
+    }
+
+    function test_BurnByOwner() public {
+        vm.prank(alice);
+        uint256 id = nft.joinOpen();
+        vm.prank(owner);
+        nft.burn(id);
+        assertEq(nft.balanceOf(alice), 0);
+    }
+
+    function test_BurnByStrangerReverts() public {
+        vm.prank(alice);
+        uint256 id = nft.joinOpen();
+        vm.prank(bob);
+        vm.expectRevert("not authorized");
+        nft.burn(id);
+    }
+
+    function test_SetJoinPolicyOnlyOwner() public {
+        vm.prank(alice);
+        vm.expectRevert();
+        nft.setJoinPolicy(MembershipNFT.JoinPolicy.Allowlist);
+        vm.prank(owner);
+        nft.setJoinPolicy(MembershipNFT.JoinPolicy.Allowlist);
+        assertEq(uint8(nft.joinPolicy()), uint8(MembershipNFT.JoinPolicy.Allowlist));
+    }
+
+    function test_SetMetadataURIOnlyOwner() public {
+        vm.prank(alice);
+        vm.expectRevert();
+        nft.setMetadataURI("ipfs://new");
+        vm.prank(owner);
+        nft.setMetadataURI("ipfs://new");
+        assertEq(nft.metadataURI(), "ipfs://new");
+    }
+
+    function test_VotesDelegation() public {
+        vm.prank(alice);
+        nft.joinOpen();
+        vm.prank(alice);
+        nft.delegate(alice);
+        assertEq(nft.getVotes(alice), 1);
+        assertEq(nft.getVotes(bob), 0);
+    }
+
+    function test_VotesDelegationToThirdParty() public {
+        vm.prank(alice);
+        nft.joinOpen();
+        vm.prank(alice);
+        nft.delegate(bob);
+        assertEq(nft.getVotes(alice), 0);
+        assertEq(nft.getVotes(bob), 1);
+    }
+
+    function test_MintToReverts_NonOwner() public {
+        vm.prank(alice);
+        vm.expectRevert();
+        nft.mintTo(bob);
+    }
+
+    function test_ApplicationPolicyBehavesLikeAllowlist() public {
+        nft = new MembershipNFT("Org", "ORG", "ipfs://m", MembershipNFT.JoinPolicy.Application, owner);
+        vm.prank(alice);
+        vm.expectRevert(MembershipNFT.NotOpen.selector);
+        nft.joinOpen();
+        vm.prank(owner);
+        nft.mintTo(alice);
+        assertEq(nft.balanceOf(alice), 1);
+    }
 }
